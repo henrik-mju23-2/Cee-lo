9d2940e76499e395d79e243c95ade9f28f4bd371
henrik <henrik.n2004@gmail.com>
2025-10-07 14:49:09 +0200

    Added ConfigurationPage. Made InformationPage into a Dialog Window

---
+++ ConfigurationPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Gaming.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Cee_lo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ConfigurationPage : Page
+    {
+        public ConfigurationPage()
+        {
+            this.InitializeComponent();
+            BlurRectangle.Visibility = Visibility.Collapsed;
+        }
+
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            InformationPopUp.IsOpen = true;
+            BlurRectangle.Visibility = Visibility.Visible;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            InformationPopUp.IsOpen = false;
+            BlurRectangle.Visibility = Visibility.Collapsed;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
