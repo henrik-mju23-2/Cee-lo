0832f6437e7b1317f61f9a6a9d3f84751f6ffb5b
henrik <henrik.n2004@gmail.com>
2025-10-14 04:22:42 +0200

    Added ResultPage

---
+++ ResultPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Cee_lo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Cee_lo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ResultPage : Page
+    {
+        public ResultPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ResultatPageBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(StartPage));
+        }
+
+        private void ResultatTextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
